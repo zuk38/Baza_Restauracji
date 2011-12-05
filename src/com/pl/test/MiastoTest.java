6878bc78ae14962deafd3574a9fdcbac92c7a1bf
Zuk <Zuk@Zuk-Komputer>
2011-12-05 22:51:47 +0100

    Pierwszy commit

---
+++ src/com/pl/test/MiastoTest.java
@@ -0,0 +1,39 @@
+package com.pl.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MiastoTest {
+
+	@Test
+	public void testdodajFirme() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWyswietlListe() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSzukaj() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEdytuj() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUsun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUsunPoz() {
+		fail("Not yet implemented");
+	}
+
+}
